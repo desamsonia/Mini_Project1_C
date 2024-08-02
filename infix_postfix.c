0aabac57117bdb8b1447954da17dc4f17ebbf266
Sonia <149040956+desamsonia@users.noreply.github.com>
2024-08-02 16:37:00 +0530

    infix_postfix.c

--- infix_postfix.c
+++ infix_postfix.c
@@ -1,7 +1,9 @@
 /* Project Title: Infix to Postfix Expression Converter and converts into a standard notation.
    Input: A+B*C
    Output1: ABC*+
-   Output2: (A+(B*C))  */
+   Output2: (A+(B*C)) 
+   
+   Using C and Stack DS */
 
 #include<stdio.h>
 #include<string.h>
