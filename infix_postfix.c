fba025c7e690e6b9a4abf27b5a3782c95208ea51
Sonia <149040956+desamsonia@users.noreply.github.com>
2024-08-02 16:36:15 +0530

    infix_postfix.c

--- infix_postfix.c
+++ infix_postfix.c
@@ -1,3 +1,8 @@
+/* Project Title: Infix to Postfix Expression Converter and converts into a standard notation.
+   Input: A+B*C
+   Output1: ABC*+
+   Output2: (A+(B*C))  */
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
